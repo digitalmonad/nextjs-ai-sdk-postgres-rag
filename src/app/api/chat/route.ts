cc9d5be4f93a876aad35c9228406e360db1c7bca
Pavel Kocman <kocmanpavel@gmail.com>
2026-03-26 23:21:12 +0100

    feat: add chat api route

---
+++ src/app/api/chat/route.ts
@@ -0,0 +1,27 @@
+import { createWorkersAI } from "workers-ai-provider";
+
+import { streamText, UIMessage, convertToModelMessages } from "ai";
+
+export async function POST(req: Request) {
+  try {
+    const { messages }: { messages: UIMessage[] } = await req.json();
+
+    const workersai = createWorkersAI({
+      accountId: process.env.CLOUDFLARE_ACCOUNT_ID!,
+      apiKey: process.env.CLOUDFLARE_API_TOKEN!,
+    });
+
+    const result = streamText({
+      model: workersai("@cf/meta/llama-3.3-70b-instruct-fp8-fast"),
+      system: "You are a helpful assistant that answers the user's questions.",
+      messages: await convertToModelMessages(messages),
+      onError(error) {
+        console.error("streamText error:", error);
+      },
+    });
+
+    return result.toUIMessageStreamResponse();
+  } catch (error) {
+    return new Response("Fail when streaming chat response", { status: 500 });
+  }
+}
